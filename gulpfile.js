5e746ddc29a2d262e98006b2c702083e6bd612d7
Baklykoff671Works <s.baklykoff@gmail.com>
2019-08-05 14:30:37 +0300

    Issue with sass:watch have been fixed

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,5 @@ gulp.task('sass', function () {
 });
  
 gulp.task('sass:watch', function () {
-  gulp.watch('./src/scss/style.scss', ['sass']);
+  gulp.watch('./src/scss/style.scss', gulp.series('sass'));
 });
