9320d65edd4935d00e93b975f3d13acd02fac74c
Baklykoff671Works <s.baklykoff@gmail.com>
2019-08-08 14:07:26 +0300

    Made separate .sass files

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', function () {
-  return gulp.src('./src/scss/style.scss')
+  return gulp.src('./src/scss/index.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./src/css'));
 });
